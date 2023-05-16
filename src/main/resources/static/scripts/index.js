0e0d5d62d592f4e9e3f59ff782a307ea051b3130
Iaroslav <eremeev.pt@gmail.com>
2023-05-16 17:02:51 +0300

    Add student getter JS part

--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -8,19 +8,4 @@ $(document).ready(function() {
         window.location.href = "cars.html";
     })
 
-    /*$.ajax({
-        url: '/student',
-        method: 'GET',
-        success: function (students) {
-            students = students.data;
-            var ul = $('<ul>');
-            $.each(students, function (key, value) {
-                var li = $('<li>').text(value.id + '. ' + value.name + ', ' +
-                    value.age + ' years, number ' + value.num + ', salary ' + value.salary);
-                ul.append(li);
-            });
-            $('body').append(ul);
-        },
-    });*/
-
 })
--- src/main/resources/static/scripts/students.js
+++ src/main/resources/static/scripts/students.js
@@ -107,4 +107,84 @@ $(document).ready(function () {
         });
     });
 
+    /* Get students */
+
+    $('#getStudentBtn').click(function () {
+        $('#getStudentForm').toggleClass('d-none');
+    });
+
+    $('#getStudentForm').submit(function (event) {
+        event.preventDefault();
+        const id = parseInt($('#getId').val());
+        const name = $('#getName').val();
+        const age = parseInt($('#getAge').val());
+        const num = parseInt($('#getNum').val());
+        const salary = parseFloat($('#getSalary').val());
+        if (!id && !name && !age && !num && !salary){
+            $.ajax({
+                url: '/student',
+                method: 'GET',
+                success: function (students) {
+                    students = students.data;
+                    var ul = $('<ul>');
+                    $.each(students, function (key, student) {
+                        var li = $('<li>').text('ID ' + student.id + '. ' + student.name + ', ' +
+                            student.age + ' years, personal number ' + student.num + ', salary ' + student.salary);
+                        ul.append(li);
+                    });
+                    $('#studentsFound').empty().append(ul);
+                },
+                error: function () {
+                    alert('Error occurred while searching for students');
+                }
+            });
+        }
+        else if (id && !name && !age && !num && !salary){
+            $.ajax({
+                url: '/student/' + id,
+                method: 'GET',
+                success: function (student) {
+                    student = student.data;
+                    var ul = $('<ul>');
+                    ul.append($('<li>').text('ID ' + student.id + '. ' + student.name + ', ' +
+                        student.age + ' years, personal number ' + student.num + ', salary ' + student.salary));
+                    $('#studentsFound').empty().append(ul);
+                },
+                error: function () {
+                    alert('Error occurred while searching for students');
+                }
+            });
+        }
+        else if (!id && (name || age || num || salary)) {
+            var searchParams = {
+                name: name,
+                age: age,
+                num: num,
+                salary: salary
+            };
+
+            $.ajax({
+                url: '/student/search',
+                method: 'GET',
+                data: searchParams,
+                success: function (students) {
+                    students = students.data;
+                    var ul = $('<ul>');
+                    $.each(students, function (key, student) {
+                        var li = $('<li>').text('ID ' + student.id + '. ' + student.name + ', ' +
+                            student.age + ' years, personal number ' + student.num + ', salary ' + student.salary);
+                        ul.append(li);
+                    });
+                    $('#studentsFound').empty().append(ul);
+                },
+                error: function () {
+                    alert('Error occurred while searching for students');
+                }
+            });
+        } else {
+            alert('Invalid input. Fill either student ID or any other parameters or leave all the fields empty')
+        }
+    })
+
+
 });
