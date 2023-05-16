b49d36480874430885831d0c0c6a1b5096953379
Iaroslav <eremeev.pt@gmail.com>
2023-05-16 17:36:29 +0300

    Delete ConstraintViolationException

--- src/main/java/com/iaroslaveremeev/exceptions/ConstraintViolationException.java
+++
@@ -1,9 +0,0 @@
-package com.iaroslaveremeev.exceptions;
-
-public class ConstraintViolationException extends Exception{
-    public ConstraintViolationException() {
-    }
-    public ConstraintViolationException(String message) {
-        super(message);
-    }
-}
