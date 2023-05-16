07996410f28b4471db6fdcfa514233f261dc16bd
Iaroslav <eremeev.pt@gmail.com>
2023-05-16 17:36:05 +0300

    Update Student class to have Hibernate annotations

--- src/main/java/com/iaroslaveremeev/model/Student.java
+++ src/main/java/com/iaroslaveremeev/model/Student.java
@@ -1,16 +1,33 @@
 package com.iaroslaveremeev.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "students", uniqueConstraints = {@UniqueConstraint(columnNames = {"name", "num"})})
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NonNull
     private String name;
+    @NonNull
     private int age;
+    @NonNull
     private int num;
+    @NonNull
     private double salary;
+
+    @OneToMany(mappedBy = "student",cascade = CascadeType.ALL)
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE)
+    @JsonIgnore
+    private List<Car> cars;
 }
