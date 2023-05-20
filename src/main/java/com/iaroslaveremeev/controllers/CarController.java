f24abd9f2602516177de602b831bdd7a7037de97
Iaroslav <eremeev.pt@gmail.com>
2023-05-20 21:31:46 +0300

    Update CarController, add comments to CarController and StudentController

--- src/main/java/com/iaroslaveremeev/controllers/CarController.java
+++ src/main/java/com/iaroslaveremeev/controllers/CarController.java
@@ -3,7 +3,6 @@ package com.iaroslaveremeev.controllers;
 import com.iaroslaveremeev.dto.ResponseResult;
 import com.iaroslaveremeev.model.Car;
 import com.iaroslaveremeev.service.CarService;
-import com.iaroslaveremeev.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,19 +13,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/car")
 public class CarController {
+    /**
+     * If a method of this Controller class is successful,
+     * the result is included in the response body.
+     * If an exception occurs, the error message is included in the response body.
+     * In each case a ResponseEntity containing the response status and result is returned.
+     */
     private CarService carService;
-    private StudentService studentService;
 
+    /**
+     * Setter method called by the Spring framework
+     * to inject the CarService instance.
+     *
+     * @param carService The CarService instance.
+     */
     @Autowired
     public void setCarService(CarService carService){
         this.carService = carService;
     }
 
-    @Autowired
-    public void setStudentService(StudentService studentService){
-        this.studentService = studentService;
-    }
-
+    /**
+     * Adds a new car.
+     *
+     * @param car The car to be added.
+     */
     @PostMapping
     public ResponseEntity<ResponseResult<Car>> add (@RequestBody Car car){
         try {
@@ -37,12 +47,20 @@ public class CarController {
         }
     }
 
+    /**
+     * Retrieves all cars.
+     */
     @GetMapping
     public ResponseEntity<ResponseResult<List<Car>>> get() {
         List<Car> cars = this.carService.get();
         return new ResponseEntity<>(new ResponseResult<>(null, cars), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a car by its ID.
+     *
+     * @param id The car ID.
+     */
     @GetMapping(path = "/{id}")
     public ResponseEntity<ResponseResult<Car>> get(@PathVariable long id) {
         try {
@@ -53,24 +71,44 @@ public class CarController {
         }
     }
 
+    /**
+     * Retrieves a list of cars by brand.
+     *
+     * @param brand The car brand name.
+     */
     @GetMapping("/search/brand")
     public ResponseEntity<ResponseResult<List<Car>>> getCarsByBrand(@RequestParam String brand) {
         return new ResponseEntity<>(new ResponseResult<>(null,
                 this.carService.getCarsByBrand(brand)), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of cars by power.
+     *
+     * @param power The power in horse powers.
+     */
     @GetMapping("/search/power")
     public ResponseEntity<ResponseResult<List<Car>>> getCarsByPower(@RequestParam Integer power) {
         return new ResponseEntity<>(new ResponseResult<>(null,
                 this.carService.getCarsByPower(power)), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of cars by year of production.
+     *
+     * @param year The year of car production.
+     */
     @GetMapping("/search/year")
     public ResponseEntity<ResponseResult<List<Car>>> getCarsByYear(@RequestParam Integer year) {
         return new ResponseEntity<>(new ResponseResult<>(null,
                 this.carService.getCarsByYear(year)), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of cars by student ID.
+     *
+     * @param studentId The student ID.
+     */
     @GetMapping("/search/studentId")
     public ResponseEntity<ResponseResult<List<Car>>> getCarsByStudentId(@RequestParam Long studentId) {
         try {
@@ -82,6 +120,11 @@ public class CarController {
         }
     }
 
+    /**
+     * Deletes a car by its ID.
+     *
+     * @param id The car ID.
+     */
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<ResponseResult<Car>> delete (@PathVariable long id){
         try {
@@ -92,6 +135,11 @@ public class CarController {
         }
     }
 
+    /**
+     * Updates a car.
+     *
+     * @param car The car to be updated.
+     */
     @PutMapping
     public ResponseEntity<ResponseResult<Car>> update (@RequestBody Car car){
         try {
--- src/main/java/com/iaroslaveremeev/controllers/StudentController.java
+++ src/main/java/com/iaroslaveremeev/controllers/StudentController.java
@@ -13,13 +13,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+    /**
+     * If a method of this Controller class is successful,
+     * the result is included in the response body.
+     * If an exception occurs, the error message is included in the response body.
+     * In each case a ResponseEntity containing the response status and result is returned.
+     */
     private StudentService studentService;
 
+    /**
+     * Setter method called by the Spring framework
+     * to inject the StudentService instance.
+     *
+     * @param studentService The StudentService instance.
+     */
     @Autowired
     public void setStudentService(StudentService studentService){
         this.studentService = studentService;
     }
 
+    /**
+     * Adds a new student.
+     *
+     * @param student The student to be added.
+     */
     @PostMapping
     public ResponseEntity<ResponseResult<Student>> add (@RequestBody Student student){
         try {
@@ -30,12 +47,20 @@ public class StudentController {
         }
     }
 
+    /**
+     * Retrieves all students.
+     */
     @GetMapping
     public ResponseEntity<ResponseResult<List<Student>>> get() {
         List<Student> students = this.studentService.get();
         return new ResponseEntity<>(new ResponseResult<>(null, students), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a student by their ID.
+     *
+     * @param id The student ID.
+     */
     @GetMapping(path = "/{id}")
     public ResponseEntity<ResponseResult<Student>> get(@PathVariable long id) {
         try {
@@ -46,30 +71,55 @@ public class StudentController {
         }
     }
 
+    /**
+     * Retrieves a list of students by first name.
+     *
+     * @param name The student's first name.
+     */
     @GetMapping("/search/name")
     public ResponseEntity<ResponseResult<List<Student>>> getStudentsByName(@RequestParam String name) {
         return new ResponseEntity<>(new ResponseResult<>(null,
                 this.studentService.getStudentsByName(name)), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of students by age.
+     *
+     * @param age The student age.
+     */
     @GetMapping("/search/age")
     public ResponseEntity<ResponseResult<List<Student>>> getStudentsByAge(@RequestParam Integer age) {
         return new ResponseEntity<>(new ResponseResult<>(null,
                 this.studentService.getStudentsByAge(age)), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of students by student personal number.
+     *
+     * @param num The student personal number.
+     */
     @GetMapping("/search/num")
     public ResponseEntity<ResponseResult<List<Student>>> getStudentsByNum(@RequestParam Integer num) {
         return new ResponseEntity<>(new ResponseResult<>(null,
                 this.studentService.getStudentsByNum(num)), HttpStatus.OK);
     }
 
+    /**
+     * Retrieves a list of students by salary.
+     *
+     * @param salary The student salary.
+     */
     @GetMapping("/search/salary")
     public ResponseEntity<ResponseResult<List<Student>>> getStudentsBySalary(@RequestParam Double salary) {
         return new ResponseEntity<>(new ResponseResult<>(null,
                 this.studentService.getStudentsBySalary(salary)), HttpStatus.OK);
     }
 
+    /**
+     * Deletes a student by their ID.
+     *
+     * @param id The student ID.
+     */
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<ResponseResult<Student>> delete (@PathVariable long id){
         try {
@@ -80,6 +130,11 @@ public class StudentController {
         }
     }
 
+    /**
+     * Updates a student.
+     *
+     * @param student The student to be updated.
+     */
     @PutMapping
     public ResponseEntity<ResponseResult<Student>> update (@RequestBody Student student){
         try {
